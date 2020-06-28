30ab98a3f16139ca4a3bf6b3cf11c72a242669d2
umibo386 <umibo386@gmail.com>
2020-06-28 09:59:34 +0900

    Initial Commit

---
+++ GitTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitTest
+//
+//  Created by 平松仁志 on 2020/06/28.
+//  Copyright © 2020 ProductTest. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
